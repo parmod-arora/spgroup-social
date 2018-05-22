960160f61430c4e78ddcaf2c29401381cec87ee9
Parmod Kumar <parmodkrarora@gmail.com>
2018-05-22 22:23:38 +0800

    Add Application.java

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# spgroup-social
+# SPGroup Social Friend Api
+
+Friend Management Api
+
+## Getting Started
+
+Demo Project for Social Friend API
+
+### Prerequisites
+
+```
+Java 8
+Maven
+
+```
+
+### Installing and Running test
+
+
+```
+git clone https://github.com/parmod-arora/spgroup-social.git
+mvn install
+```
+mvn install will create build and run test
+
+
+### Approach
+
+1) Create maven project in Eclipse.
+2) Add spring-boot dependencies Web, JPA, H2database (https://start.spring.io/)
+3) Install dependencies using maven refesh from IDE or run mvn install and wait for jars to download 
+4) Create domain model for Friend 
--- src/main/java/com/spgroup/social/Application.java
+++ src/main/java/com/spgroup/social/Application.java
@@ -1,14 +1,7 @@
 package com.spgroup.social;
 
-import java.util.stream.IntStream;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.spgroup.social.dao.UserRepository;
-import com.spgroup.social.dao.domain.User;
 
 @SpringBootApplication
 public class Application {
@@ -17,14 +10,4 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 	
-	
-	@Bean
-	public CommandLineRunner loadData(UserRepository repository) {
-	    return (args) -> {
-	    	IntStream.rangeClosed(1, 20).forEach(num -> {
-	    		repository.save(new User("test"+num+"@test.com"));
-	    	});
-	    };
-	}
-
 }
